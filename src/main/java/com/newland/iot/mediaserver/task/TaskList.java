c1073c15c8dc7549624d26e18628d7ddf943ef09
bambooq <cna_ren@foxmail.com>
2017-08-04 15:03:42 +0800

    开启定时任务的并发执行特性

--- src/main/java/com/newland/iot/mediaserver/task/TaskList.java
+++ src/main/java/com/newland/iot/mediaserver/task/TaskList.java
@@ -4,10 +4,14 @@ import com.newland.iot.mediaserver.service.user.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.Executors;
+
 @Component
-public class TaskList {
+public class TaskList implements SchedulingConfigurer {
 
 	private final static Logger log = Logger.getLogger(TaskList.class);
 
@@ -19,8 +23,15 @@ public class TaskList {
 	 */
 	@Scheduled(fixedRate = 1000 * 300)
 	public void doUpdateDictDef() {
-
+		log.info("定时任务");
 	}
 
 
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		/**
+		 * 开启并发执行
+		 */
+		taskRegistrar.setScheduler(Executors.newScheduledThreadPool(100));
+	}
 }
